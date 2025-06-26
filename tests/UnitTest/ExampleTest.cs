bec68e35f283212077fed55a9275ff181356ddb3
刘宇然 <liuyuran719829142@hotmail.com>
2025-06-26 10:13:15 +0800

    add unit test support

---
+++ tests/UnitTest/ExampleTest.cs
@@ -0,0 +1,31 @@
+﻿using GdUnit4;
+using static GdUnit4.Assertions;
+
+namespace UnitTest;
+
+[TestSuite]
+public class ExampleTest
+{
+    public static IEnumerable<object[]> arrayDataPointProperty => [[1, 2, 3], [4, 5, 9]];
+    public static IEnumerable<object[]> ArrayDataPointMethod() => [[1, 2, 3], [4, 5, 9]];
+
+    [TestCase]
+    public void Success() => AssertBool(true).IsTrue();
+
+
+    [TestCase]
+    public void Failed() => AssertBool(false).IsTrue();
+
+    [TestCase]
+    [DataPoint(nameof(arrayDataPointProperty))]
+    public void WithDataPointProperty(int a, int b, int expected) => AssertThat(a + b).IsEqual(expected);
+
+    [TestCase]
+    [DataPoint(nameof(ArrayDataPointMethod))]
+    public void WithArrayDataPointMethod(int a, int b, int expected) => AssertThat(a + b).IsEqual(expected);
+
+
+    [TestCase(0, 1, 2, TestName = "TestA")]
+    [TestCase(1, 2, 3, TestName = "TestB", Description = "foo ")]
+    public void DataRows(int a, int b, int c) => AssertBool(true).IsTrue();
+}
