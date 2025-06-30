4b75aee51f6bdcf29a775a8ea732314cca2daca0
刘宇然 <liuyuran719829142@hotmail.com>
2025-06-30 11:24:00 +0800

    Adds ECS bridge test to unit tests
    Adds a new unit test that tests the ECS bridge functionality.
    
    Replaces the RichTextLabel with a ScrollContainer to accommodate the log output.

---
+++ src/clientv4/unittest/ECSPart.cs
@@ -0,0 +1,23 @@
+﻿using game.scripts.server.ECSBridge;
+
+namespace game.unittest;
+
+public partial class ShownUnitTest {
+    private ECSSystemBridge _bridgeNode;
+    
+    private void RunTestForECSBridge() {
+        _bridgeNode = new ECSSystemBridge();
+        _boxGame.AddChild(_bridgeNode);
+        AppendLog("Running ECS Bridge Test...");
+    }
+    
+    private void CleanupECSBridgeTest() {
+        if (_bridgeNode != null) {
+            _bridgeNode.QueueFree();
+            _bridgeNode = null;
+            AppendLog("ECS Bridge Test Cleaned Up.");
+        } else {
+            AppendLog("No ECS Bridge Test to clean up.");
+        }
+    }
+}
--- src/clientv4/unittest/ShownUnitTest.cs
+++ src/clientv4/unittest/ShownUnitTest.cs
@@ -1,4 +1,7 @@
-﻿using Godot;
+﻿using System;
+using System.Collections.Generic;
+using game.scripts.config;
+using Godot;
 
 namespace game.unittest;
 
@@ -7,10 +10,80 @@ namespace game.unittest;
 /// </summary>
 public partial class ShownUnitTest: Control {
     private Node _box;
-    private RichTextLabel _result;
+    private Control _boxUI;
+    private Node3D _boxGame;
+    private ScrollContainer _result;
+    private VBoxContainer _logBox;
+    private readonly List<Action> _test = [];
+    private readonly List<Action> _testClean = [];
+    private int _currentTestIndex;
 
     public override void _Ready() {
         _box = GetNode<Node>("Box");
-        _result = GetNode<RichTextLabel>("Result");
+        _boxUI = new Control();
+        _boxGame = new Node3D();
+        _box.AddChild(_boxUI);
+        _box.AddChild(_boxGame);
+        _boxUI.Visible = false;
+        _boxGame.Visible = false;
+        _result = GetNode<ScrollContainer>("Result");
+        _logBox = new VBoxContainer();
+        _result.AddChild(_logBox);
+        _result.VerticalScrollMode = ScrollContainer.ScrollMode.ShowAlways;
+        _result.HorizontalScrollMode = ScrollContainer.ScrollMode.Disabled;
+        CallDeferred(MethodName.OnRootSizeChanged);
+        GetTree().Root.SizeChanged += OnRootSizeChanged;
+        AddTest(RunTestForECSBridge, CleanupECSBridgeTest);
+    }
+
+    private void AddTest(Action main, Action clean) {
+        _test.Add(main);
+        _testClean.Add(clean);
+    }
+
+    private void OnRootSizeChanged() {
+        _result.Size = GetTree().Root.Size - new Vector2I(20, 0);
+        _result.Position = new Vector2(10, 0);
+        for (var i = 0; i < _result.GetChildCount(); i++) {
+            var child = _result.GetChild(i);
+            if (child is RichTextLabel label) {
+                label.CustomMinimumSize = new Vector2(_result.Size.X, 30);
+            }
+        }
+    }
+    
+    private void AppendLog(string message) {
+        var label = new RichTextLabel {
+            Text = message,
+            SizeFlagsHorizontal = SizeFlags.ExpandFill,
+            SizeFlagsVertical = SizeFlags.ShrinkCenter,
+            FitContent = true,
+            BbcodeEnabled = true,
+            CustomMinimumSize = new Vector2(_result.Size.X, 30),
+            AutowrapMode = TextServer.AutowrapMode.Word,
+        };
+        _logBox.AddChild(label);
+        _logBox.CustomMinimumSize = _logBox.Size += new Vector2(0, label.Size.Y);
+        _result.ScrollVertical = (int)_logBox.CustomMinimumSize.Y;
+    }
+
+    public override void _Input(InputEvent @event) {
+        InputManager.instance.HandleInputEvent(@event);
+    }
+
+    public override void _Process(double delta) {
+        var wheel = InputManager.instance.GetMouseWheelAndReset();
+        if (wheel != 0) {
+            _result.ScrollVertical += (int)(wheel * 10);
+        }
+
+        if (_currentTestIndex >= _test.Count) {
+            return;
+        }
+        
+        _test[_currentTestIndex]?.Invoke();
+        _testClean[_currentTestIndex]?.Invoke();
+
+        _currentTestIndex++;
     }
 }
