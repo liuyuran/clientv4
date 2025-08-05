d9b825f5003d70a77f38ec1ed065dbdc28046b56
Miata <liuyuran719829142@hotmail.com>
2025-08-05 22:18:32 +0800

    revert

--- src/clientv4/scripts/gui/InGameUI/InGamingUI.cs
+++ src/clientv4/scripts/gui/InGameUI/InGamingUI.cs
@@ -6,54 +6,58 @@ using Godot;
 namespace game.scripts.gui.InGameUI;
 
 public partial class InGamingUI: CanvasLayer {
-	[Export] public PackedScene PlayingUI;
-	[Export] public PackedScene PauseUI;
-	[Export] public PackedScene MenusUI;
-	private InGamingUIStatus _status;
-	private LineEdit _msgInput;
+    [Export] public PackedScene PlayingUI;
+    [Export] public PackedScene PauseUI;
+    [Export] public PackedScene MenusUI;
+    private InGamingUIStatus _status;
+    private LineEdit _msgInput;
+    private ulong _lastActiveInputTime;
+    private const ulong MinInputInterval = 500;
 		
-	public override void _Ready() {
-		GameNodeReference.UI = this;
-		ProcessMode = ProcessModeEnum.Always;
-		_status.Focus = InGameUIFocus.Game;
-		OpenPlayingUI();
-		_msgInput = this.FindNodeByName<LineEdit>("MsgInput");
-	}
+    public override void _Ready() {
+        GameNodeReference.UI = this;
+        ProcessMode = ProcessModeEnum.Always;
+        _status.Focus = InGameUIFocus.Game;
+        OpenPlayingUI();
+        _msgInput = this.FindNodeByName<LineEdit>("MsgInput");
+    }
 
-	public override void _Process(double delta) {
-		switch (_status.Focus) {
-			case InGameUIFocus.Game:
-				UpdatePlayingUI(delta);
-				TryOpenPauseUI();
-				break;
-			case InGameUIFocus.Pause:
-				TryClosePauseUI();
-				break;
-			default:
-				throw new ArgumentOutOfRangeException();
-		}
-	}
+    public override void _Process(double delta) {
+        switch (_status.Focus) {
+            case InGameUIFocus.Game:
+                UpdatePlayingUI(delta);
+                TryOpenPauseUI();
+                break;
+            case InGameUIFocus.Pause:
+                TryClosePauseUI();
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+        if (_status.Focus != InGameUIFocus.Game && 
+            GameStatus.currentStatus != GameStatus.Status.Playing &&
+            GameStatus.currentStatus != GameStatus.Status.Typing) return;
+        if (!InputManager.instance.IsKeyPressed(InputKey.UIConfirm)) return;
+        if (PlatformUtil.GetTimestamp() - _lastActiveInputTime < MinInputInterval) return;
+        _lastActiveInputTime = PlatformUtil.GetTimestamp();
+        if (_msgInput.HasFocus()) {
+            _msgInput.ReleaseFocus();
+            GameStatus.SetStatus(GameStatus.Status.Playing);
+        } else {
+            _msgInput.GrabFocus();
+            GameStatus.SetStatus(GameStatus.Status.Typing);
+        }
+    }
 
-	public override void _Input(InputEvent @event) {
-		switch (_status.Focus) {
-			case InGameUIFocus.Game:
-				HandleInputOnPlayerUI(@event);
-				break;
-			case InGameUIFocus.Pause:
-				break;
-			default:
-				throw new ArgumentOutOfRangeException();
-		}
-		if (_status.Focus != InGameUIFocus.Game && 
-		    GameStatus.currentStatus != GameStatus.Status.Playing &&
-		    GameStatus.currentStatus != GameStatus.Status.Typing) return;
-		if (!InputManager.instance.IsKeyPressed(InputKey.UIConfirm, @event)) return;
-		if (_msgInput.HasFocus()) {
-			_msgInput.ReleaseFocus();
-			GameStatus.SetStatus(GameStatus.Status.Playing);
-		} else {
-			_msgInput.GrabFocus();
-			GameStatus.SetStatus(GameStatus.Status.Typing);
-		}
-	}
+    public override void _Input(InputEvent @event) {
+        switch (_status.Focus) {
+            case InGameUIFocus.Game:
+                HandleInputOnPlayerUI(@event);
+                break;
+            case InGameUIFocus.Pause:
+                break;
+            default:
+                throw new ArgumentOutOfRangeException();
+        }
+    }
 }
