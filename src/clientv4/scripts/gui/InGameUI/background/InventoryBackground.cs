8a456b04baabad6342e350d7ed2ecd0bbdb83fe0
Miata <liuyuran719829142@hotmail.com>
2025-08-24 18:41:16 +0800

    Adds inventory system and action bar
    Introduces the inventory system with item display.
    
    Implements action bar for quick item access, with both keyboard and gamepad support.
    
    Refactors inventory background script and introduces a global theme.

--- src/clientv4/scripts/gui/InGameUI/background/InventoryBackground.cs
+++
@@ -1,9 +0,0 @@
-﻿using Godot;
-
-namespace game.scripts.gui.InGameUI.background;
-
-public partial class InventoryBackground: Panel {
-    public override void _Draw() {
-        //
-    }
-}
---
+++ src/clientv4/scripts/gui/InGameUI/component/Inventory.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+
+namespace game.scripts.gui.InGameUI.component;
+
+public partial class Inventory: Control {
+    [Export] private PackedScene _inventoryItem;
+
+    public override void _Ready() {
+        //
+    }
+
+    public override void _Process(double delta) {
+        //
+    }
+
+    public override void _Draw() {
+        //
+    }
+}
